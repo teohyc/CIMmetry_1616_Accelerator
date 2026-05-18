97aebe11d977feca0d295ea9658555fe87b23a10
Teoh Yung Ching <teohyungching@gmail.com>
2026-05-18 13:04:33 +0800

    Add files via upload

---
+++ Quartus-Eclipse/cimmetry_1616_software_C/siamese_cnn_32x32_noip/obj/default/main.d
@@ -0,0 +1,27 @@
+obj/default/main.o: main.c ../cimmetry1616_bsp_temp_2_bsp/system.h \
+ ../cimmetry1616_bsp_temp_2_bsp/linker.h \
+ ../cimmetry1616_bsp_temp_2_bsp//HAL/inc/sys/alt_timestamp.h \
+ ../cimmetry1616_bsp_temp_2_bsp//HAL/inc/alt_types.h \
+ ../cimmetry1616_bsp_temp_2_bsp//drivers/inc/altera_avalon_timer.h \
+ ../cimmetry1616_bsp_temp_2_bsp//HAL/inc/sys/alt_dev.h \
+ ../cimmetry1616_bsp_temp_2_bsp//HAL/inc/sys/alt_llist.h \
+ ../cimmetry1616_bsp_temp_2_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../cimmetry1616_bsp_temp_2_bsp//HAL/inc/sys/alt_warning.h
+
+../cimmetry1616_bsp_temp_2_bsp/system.h:
+
+../cimmetry1616_bsp_temp_2_bsp/linker.h:
+
+../cimmetry1616_bsp_temp_2_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../cimmetry1616_bsp_temp_2_bsp//HAL/inc/alt_types.h:
+
+../cimmetry1616_bsp_temp_2_bsp//drivers/inc/altera_avalon_timer.h:
+
+../cimmetry1616_bsp_temp_2_bsp//HAL/inc/sys/alt_dev.h:
+
+../cimmetry1616_bsp_temp_2_bsp//HAL/inc/sys/alt_llist.h:
+
+../cimmetry1616_bsp_temp_2_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../cimmetry1616_bsp_temp_2_bsp//HAL/inc/sys/alt_warning.h:
